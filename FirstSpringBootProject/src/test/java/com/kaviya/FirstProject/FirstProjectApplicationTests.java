7af3cf3860475521297b7cee44823325cfa96234
Kaviya G <167405024+Kaviya12223@users.noreply.github.com>
2025-07-03 20:04:48 +0530

    Add files via upload

---
+++ FirstSpringBootProject/src/test/java/com/kaviya/FirstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaviya.FirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
